ed9f0c528322787948e8b5a4c22f9073f6a90057
Dzov <amelie.haladjian@gmail.com>
2017-02-27 11:17:36 +0100

    commentaires

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+// Création de l'élément title
 let head = document.getElementsByTagName("head")[0]; 
 let titleTag = document.createElement("title"); 
 let title = document.createTextNode("Exercice JavaScript");

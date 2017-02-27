efeaa1bf39e9f32818b7c908f7961cc44008f247
abdoulaye <abdoulayedabo@laposte.net>
2017-02-27 12:04:28 +0100

    Partie d'Abdoulaye, modifications de la 1ere section

--- js/script.js
+++ js/script.js
@@ -15,20 +15,20 @@ body.appendChild(main);
 
 // la partie de Abdoulaye
 let section = document.createElement("section"); // crée la balise section
-document.body.appendChild(section); // rajoute la section à body
+document.body.appendChild(section); // rajoute la section au main
 
 let header = document.createElement("header"); // crée la balise header
-document.body.appendChild(header); // rajoute le header au body
+section.appendChild(header); // rajoute le header à la section
 
-let h2 = document.createElement("title"); // crée un titre de niveau h2
-document.body.appendChild(h2); // rajoute un titre de niveau h2
+let h2 = document.createElement("h2"); // crée un titre de niveau h2
+header.appendChild(h2); // rajoute un titre de niveau h2
 
 let contentH2 = document.createTextNode("Lorem ipsum"); // crée un contenu pour le h2
 h2.appendChild(contentH2); // rajoute le contenu au h2
-document.body.appendChild(contentH2); // rajoute le h2 et son contenu au body
 
-let paragraph = document.createElement("paragraph"); // crée un paragraphe
-let contentParagraph = document.createTextNode("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus."); /* crée
+let paragraph = document.createElement("p"); // crée un paragraphe
+let contentParagraph = document.createTextNode("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.");
+/* crée
 un contenu pour le paragraphe */
-paragraph.appendChild(paragraph); // rajoute le contenu au paragraphe
-document.body.appendChild(paragraph); // rajoute le paragraphe et son contenu au body
+paragraph.appendChild(contentParagraph); // rajoute le contenu au paragraphe
+section.appendChild(paragraph); // rajoute le paragraphe et son contenu au body
